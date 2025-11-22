ce8b42f1c2fdecce378fde96156cff5afd2bd0d2
Scarlett253 <yvonnarriola@gmail.com>
2025-11-21 19:16:57 -0500

    my code isn't coding!!!!!
    finishb for today

--- finalProject211/to-be-seen-to-disappear/script.js
+++ finalProject211/to-be-seen-to-disappear/script.js
@@ -4,4 +4,16 @@ CART 211 - Final Project*/
 //Likes page4
 function like() {
     let c = document.getElementById("count");
+    let n = number(c.innerText);
+    c.innerText = n + 1;
 }
+
+//Fade out image page9
+function fadeOutImage() {
+    let img = document.getElementById("fadeImage");
+    img.style.opacity = 0;
+    setTimeout(() => {
+        window.location.href = "page10.html";
+    }, 4000);
+}
+
